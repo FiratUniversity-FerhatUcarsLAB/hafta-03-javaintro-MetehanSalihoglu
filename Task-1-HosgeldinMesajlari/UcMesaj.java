03b4b128d5c6eb2e4826b3a6b3236968f1ef1500
MetehanSalihoglu <101469728+MetehanSalihoglu@users.noreply.github.com>
2025-10-22 21:34:34 +0300

    Update UcMesaj.java

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,13 @@
+25542025
+Metehan Salihoğlu
+    
+
+public class UcMesaj {
+    public static void main(String[] args)  {
+        System.out.println("Javaya Hoş Geldiniz");
+        System.out.println("Bilgisayar Bilimine HoşGeldiniz")
+        System.out.println("Proglamlama Eğlencelidir");
+        
+    }
+
+}
