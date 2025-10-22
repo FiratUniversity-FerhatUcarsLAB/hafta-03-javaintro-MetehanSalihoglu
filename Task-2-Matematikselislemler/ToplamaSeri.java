1966c25050d90e3a531716b970c95dfcd31d05a8
MetehanSalihoglu <101469728+MetehanSalihoglu@users.noreply.github.com>
2025-10-22 21:36:45 +0300

    Update ToplamaSeri.java

---
+++ Task-2-Matematikselislemler/ToplamaSeri.java
@@ -0,0 +1,12 @@
+250542025
+  Metehan Salihoğlu
+
+    public class ToplamaSerijava {
+    public static void main(String[] args) {
+     int x=1+2+3+4+5+6+7+8+9;
+     // x'i hesapla (bu bir yorum)
+     System.out.println("1+2+3+4+5+6+7+8+9=" +x);
+     //yazdır
+    }
+
+}
