598a9c5185c8c11139af1d0de8207624bf2d7095
MetehanSalihoglu <101469728+MetehanSalihoglu@users.noreply.github.com>
2025-10-22 21:33:07 +0300

    Update BesKereTekrar.java

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,15 @@
+250542025
+  Metehan Salihoğlu
+
+public class BesKereTekrar {
+    public static void main(String[] args) {
+     
+        System.out.println("Javaya Hoş Geldiniz");
+        System.out.println("Javaya Hoş Geldiniz");
+        System.out.println("Javaya Hoş Geldiniz");
+        System.out.println("Javaya Hoş Geldiniz");
+        System.out.println("Javaya Hoş Geldiniz");
+
+    }
+
+}
