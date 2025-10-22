e63f61d64a523cd1087b9753562d57b1fac47544
MetehanSalihoglu <101469728+MetehanSalihoglu@users.noreply.github.com>
2025-10-22 21:45:47 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,20 @@
+250542025
+  Metehan Salihoğlu
+
+     public class İsimKarti {
+    public static void main(String[] args) {
+    String ad= "Metehan";
+    String soyad="Salihoğlu";
+    String ogrno="250542025";
+    String bolum="Yazılım MÜhendisliği";
+
+    System.out.println("----------------");
+    System.out.println("Ogrenci bilgileri");
+    System.out.println("-----------------------");
+    System.out.println("Ad :" + ad);
+    System.out.println("Soyad  :"  +soyad);
+    System.out.println("No :" +ogrno);
+    System.out.println("Bolum :"+bolum);
+     System.out.println("--------------");
+    }
+}
