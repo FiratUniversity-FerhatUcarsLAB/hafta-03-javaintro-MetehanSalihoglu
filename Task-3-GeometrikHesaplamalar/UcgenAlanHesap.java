2bd7d8829a16b6ce9f67e2b5a4f28396fe22a05b
MetehanSalihoglu <101469728+MetehanSalihoglu@users.noreply.github.com>
2025-10-22 21:38:54 +0300

    Update UcgenAlanHesap.java

---
+++ Task-3-GeometrikHesaplamalar/UcgenAlanHesap.java
@@ -0,0 +1,19 @@
+250542025
+  Metehan Salihoğlu
+
+     public class UcgenHesap {
+    public static void main(String[] args) {
+  int a=3;
+  int b=4;
+  int c=5;
+    int cevre=a+b+c;
+    double s=(a+b+c)/2;
+    double alan=Math.sqrt(s*(s-a)*(s-b)*(s-c))
+    ;
+
+
+    System.out.println("Cevre :" +cevre);
+    System.out.println("Alan :" +alan);
+
+      }
+      }
