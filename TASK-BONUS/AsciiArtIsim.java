84c02b6a79625c6c60aa4b4fb9ccf2c702568d5a
MetehanSalihoglu <101469728+MetehanSalihoglu@users.noreply.github.com>
2025-10-22 22:00:42 +0300

    Update AsciiArtIsim.java

---
+++ TASK-BONUS/AsciiArtIsim.java
@@ -0,0 +1,21 @@
+250542025
+  Metehan Salihoğlu
+
+
+    
+public class AsciiArtIsim {
+
+    public static void main(String[] args) {
+        yazdirAsciiIsim();
+    }
+
+
+    public static void yazdirAsciiIsim() {
+        System.out.println(" __  __      _        _             _     _ ");
+        System.out.println("|  \\/  | ___| |_ __ _| | ___  _   _| |__ (_)");
+        System.out.println("| |\\/| |/ _ \\ __/ _` | |/ _ \\| | | | '_ \\| |");
+        System.out.println("| |  | |  __/ || (_| | | (_) | |_| | |_) | |");
+        System.out.println("|_|  |_|\\___|\\__\\__,_|_|\\___/ \\__,_|_.__/|_|");
+        System.out.println("\n\tM E T E H A N");
+    }
+}
