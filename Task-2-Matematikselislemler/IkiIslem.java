4d69564df3b3c1fd2acba9dd710256a34e1b6de3
MetehanSalihoglu <101469728+MetehanSalihoglu@users.noreply.github.com>
2025-10-22 21:36:11 +0300

    Update IkiIslem.java

---
+++ Task-2-Matematikselislemler/IkiIslem.java
@@ -0,0 +1,12 @@
+250542025
+  Metehan Salihoğlu
+
+    public class İkiislem {
+    public static void main(String[] args) {
+     double pay= (9.5*4.5)-(2.5*3);
+     double payda= 45.5-3.5;
+     double sonuc= pay / payda;
+     System.out.printf("%4f\n", sonuc);
+
+    }
+}
