b34b706093e03f986a7df16a05bd25cafe43eaa1
MetehanSalihoglu <101469728+MetehanSalihoglu@users.noreply.github.com>
2025-10-22 21:33:45 +0300

    Update JavaDeseni.java

---
+++ Task-1-HosgeldinMesajlari/JavaDeseni.java
@@ -0,0 +1,14 @@
+250542025
+  Metehan Salihoğlu
+    
+public class JavaDeseni {
+    public static void main(String[] args)  {
+     
+        System.out.println("   J A V V A  ");
+        System.out.println(" J A A V V A A");
+        System.out.println("J J AAAA V V AAAA  ");
+        System.out.println(" J J A A V A A  ");
+
+    }
+
+}
