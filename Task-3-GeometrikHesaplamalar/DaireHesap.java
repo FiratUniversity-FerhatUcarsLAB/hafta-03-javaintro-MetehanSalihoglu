61e1a83c811a18782821e02420a8fa92cc4bc6f4
MetehanSalihoglu <101469728+MetehanSalihoglu@users.noreply.github.com>
2025-10-22 21:37:40 +0300

    Update DaireHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DaireHesap.java
@@ -0,0 +1,15 @@
+250542025
+  Metehan Salihoğlu
+
+     public class DaireHesapla {
+    public static void main(String[] args) {
+        double r = 5.5;
+        double pi = 3.14;
+        double cevre = 2 * pi * r;
+        double alan = pi * r * r;
+
+        System.out.println("yaricap: " + r);
+        System.out.println("cevre: " + cevre);
+        System.out.println("alan: " + alan);
+    }
+}
