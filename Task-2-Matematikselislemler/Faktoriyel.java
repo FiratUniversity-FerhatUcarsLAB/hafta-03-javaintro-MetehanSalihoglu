d1ee3571b7d8d552d1d45b7112a94aee5a49e873
MetehanSalihoglu <101469728+MetehanSalihoglu@users.noreply.github.com>
2025-10-22 21:35:31 +0300

    Update Faktoriyel.java

---
+++ Task-2-Matematikselislemler/Faktoriyel.java
@@ -0,0 +1,11 @@
+250542025
+  Metehan Salihoğlu
+
+    public class Faktoriyeljava {
+    public static void main(String[] args) {
+     int x=5*4*3*2*1;
+     System.out.println("5!=5*4*3*2*1="+x);
+    }
+
+}
+
